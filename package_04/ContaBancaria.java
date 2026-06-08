d6b19605c6d711f0315dbb7ad6727c33c8abf355
PedroCollares <202313600062@ifba.edu.br>
2026-06-07 21:20:01 -0300

    Update ContaBancaria.java

--- package_04/ContaBancaria.java
+++ package_04/ContaBancaria.java
@@ -5,33 +5,21 @@ public class ContaBancaria {
     private String titular;
     private int valor_depositado;
 
-    public int getValor_depositado() {
-        return valor_depositado;
-    }
-
-    public void setValor_depositado(int valor_depositado) {
-        this.valor_depositado = valor_depositado;
-    }
-
     public ContaBancaria(int saldo, String titular) {
         this.Saldo = saldo;
         this.titular = titular;
+    }
+
+    public int getValor_depositado() {
+        return valor_depositado;
     }
 
     public int getSaldo() {
         return Saldo;
     }
 
-    public void setSaldo(int saldo) {
-        Saldo = saldo;
-    }
-
     public String getTitular() {
         return titular;
-    }
-
-    public void setTitular(String titular) {
-        this.titular = titular;
     }
 
     public int SaldoCliente(int valor_depositado) {
